eabba847f594d48fc9568950018afd759b3f0cd7
Brian Estrada <165035871+BrianXD1504@users.noreply.github.com>
2025-09-10 01:36:53 -0300

    ahora el index.js tenia que guardar como y poder el js nuevo en src

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// tenemos en arreglo vacio 
+let mascotas = [];
+
+// los datos de entrada 
+function registrarMascota(nombre, tipo, edad, duenio, vacunada) {
+// crar los objetos 
+  let mascota = {
+    nombre: nombre,
+    tipo: tipo,
+    edad: edad,
+    duenio: duenio,
+    vacunada: vacunada
+  };
+
+// gaardamos los odjetos de la arreglo "final de arreglo"
+  mascotas.push(mascota);
+
+  // para verificar usamos consola para que se muestre en el navegador
+  console.log("Mascota registrada:", mascota);
+  console.log("Lista completa:", mascotas);
+}
+
+// a aqui son 2 eje. es como el registo de mascotas 
+registrarMascota("Luna", "Gato", 3, "Juan Perez", true);
+registrarMascota("Firulais", "Perro", 5, "Ana Gómez", false);
